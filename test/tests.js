a55a6ed5ff621f78cf2df81e88953348ccfe60f9
John-Henry Liberty <johnhenry.liberty@gmail.com>
2015-06-13 11:50:47 -0500

    adds tests for aliases

--- test/tests.js
+++ test/tests.js
@@ -7,23 +7,11 @@ var join = require('path').join;
 var mc;
 var cli;
 var attrs;
+var aliasesClass;
 var emberActual;
 var emberExpected;
 var sailsActual;
 var sailsExpected;
-
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      expect(-1).to.equal([1, 2, 3].indexOf(5));
-      expect(-1).to.equal([1, 2, 3].indexOf(0));
-    });
-
-    it('should return the proper index when the value is present', function(){
-      expect(0).equal([1, 2, 3].indexOf(1));
-    });
-  });
-});
 
 describe('Seeds', function() {
   it('should return a function', function() {
@@ -60,6 +48,13 @@ describe('ModelConverter', function() {
   });
 
   it('for Ember should contain only Ember datatypes', function() {
+    expect(emberActual).to.match(/string/i);
+    expect(emberActual).to.match(/date/i);
+    expect(emberActual).to.match(/number/i);
+    expect(emberActual).to.match(/boolean/i);
+  });
+
+  it('for Ember should not contain non-Ember datatypes', function() {
     expect(emberActual).not.to.match(/datatime/i);
     expect(emberActual).not.to.match(/binary/i);
     expect(emberActual).not.to.match(/json/i);
@@ -68,20 +63,19 @@ describe('ModelConverter', function() {
     expect(emberActual).not.to.match(/float/i);
     expect(emberActual).not.to.match(/integer/i);
     expect(emberActual).not.to.match(/text/i);
-    expect(emberActual).to.match(/string/i);
-    expect(emberActual).to.match(/date/i);
-    expect(emberActual).to.match(/number/i);
-    expect(emberActual).to.match(/boolean/i);
   });
 
   it('should properly convert the datatypes for sails', function() {
     expect(sailsActual).to.eql(sailsExpected);
   });
 
-  it('for Sails should contain only Sails datatypes', function() {
+  it('for Sails should have default datatypes that map to Sails', function() {
     expect(sailsActual).not.to.match(/datetime/i);
     expect(sailsActual).not.to.match(/float/i);
     expect(sailsActual).not.to.match(/number/i);
+  });
+
+  it('for Sails should contain only Sails datatypes', function() {
     expect(sailsActual).to.match(/binary/i);
     expect(sailsActual).to.match(/json/i);
     expect(sailsActual).to.match(/binary/i);
@@ -90,6 +84,62 @@ describe('ModelConverter', function() {
     expect(sailsActual).to.match(/string/i);
     expect(sailsActual).to.match(/date/i);
     expect(sailsActual).to.match(/boolean/i);
+  });
+});
+
+describe('Aliases', function() {
+  before(function() {
+    aliasesClass = require(join('..', 'lib', 'settings', 'aliases'));
+  });
+
+  it('should be a class function', function() {
+    expect(aliasesClass).to.be.a('function');
+  });
+
+  it('should accept aliases for server', function() {
+    let serve = ['serve'];
+    let s = Seeds.cli(['s']).args;
+    let server = Seeds.cli(['server']).args;
+    let lift = Seeds.cli(['lift']).args;
+    let up = Seeds.cli(['up']).args;
+    expect(s).to.eql(serve);
+    expect(server).to.eql(serve);
+    expect(lift).to.eql(serve);
+    expect(up).to.eql(serve);
+  });
+
+  it('should accept alias for generate', function() {
+    let generate = ['generate'];
+    let g = Seeds.cli(['g']).args;
+    expect(g).to.eql(generate);
+  });
+
+  it('should accept alias for new', function() {
+    let newArg = ['new'];
+    let n = Seeds.cli(['n']).args;
+    expect(n).to.eql(newArg);
+  });
+
+  it('should accept flags for version', function() {
+    let dashV = Seeds.cli(['-v']).args;
+    let ddVersion = Seeds.cli(['--version']).args;
+    let version = ['version'];
+    expect(dashV).to.eql(version);
+    expect(ddVersion).to.eql(version);
+  });
+
+  it('should accept flags for help', function() {
+    let help = ['help'];
+    let dashH = Seeds.cli(['-h']).args;
+    let ddHelp = Seeds.cli(['--help']).args;
+    expect(dashH).to.eql(help);
+    expect(ddHelp).to.eql(help);
+  });
+
+  it('should prioritize flags over aliases/commands', function() {
+    let flagOverAlias = Seeds.cli(['n', '--version']).args;
+    let flagOA = ['version', '--version'];
+    expect(flagOverAlias).to.eql(flagOA);
   });
 });
 
